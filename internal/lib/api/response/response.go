f98643e8045d904cd21b6c798f315628be09981a
4aykovski <kan89kan@gmail.com>
2024-02-24 11:08:35 +0300

    feat: more validation errors

--- internal/lib/api/response/response.go
+++ internal/lib/api/response/response.go
@@ -39,6 +39,12 @@ func ValidationError(errs validator.ValidationErrors) Response {
 			errMsgs = append(errMsgs, fmt.Sprintf("field %s is a required field", err.Field()))
 		case "url":
 			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not a valid URL", err.Field()))
+		case "min":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s must be longer than %s symbols", err.Field(), err.Param()))
+		case "max":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s must be smaller than %s symbols", err.Field(), err.Param()))
+		case "containsany":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s must contains any of special character", err.Field()))
 		default:
 			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
 		}
