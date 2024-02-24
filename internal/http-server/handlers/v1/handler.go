3392535e5d3855b655cd70e2801b02e3a60e9062
4aykovski <kan89kan@gmail.com>
2024-02-24 11:08:58 +0300

    feat: add userService in Handler struct

--- internal/http-server/handlers/v1/handler.go
+++ internal/http-server/handlers/v1/handler.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"context"
 	"log/slog"
 
 	customMiddleware "github.com/4aykovski/learning/golang/rest/internal/http-server/middleware"
+	"github.com/4aykovski/learning/golang/rest/internal/services"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -15,16 +17,28 @@ type UrlRepository interface {
 	DeleteURL(alias string) error
 }
 
-type Handler struct {
-	UrlRepo UrlRepository
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name UserService
+type UserService interface {
+	SignUp(ctx context.Context, input services.UserSignUpInput) error
 }
 
-func New(urlRepo UrlRepository) *Handler {
-	return &Handler{UrlRepo: urlRepo}
+type Handler struct {
+	UrlRepo     UrlRepository
+	UserService UserService
+}
+
+func New(urlRepo UrlRepository, userService UserService) *Handler {
+	return &Handler{
+		UrlRepo:     urlRepo,
+		UserService: userService,
+	}
 }
 
 func (h *Handler) InitRoutes(log *slog.Logger, r *chi.Mux) {
-	h.initUrlRoutes(log, r)
+	r.Route("/api/v1", func(r chi.Router) {
+		h.initUrlRoutes(log, r)
+		h.initUserRoutes(log, r)
+	})
 }
 
 func (h *Handler) InitMiddlewares(log *slog.Logger, r *chi.Mux) {
@@ -35,11 +49,25 @@ func (h *Handler) InitMiddlewares(log *slog.Logger, r *chi.Mux) {
 	r.Use(middleware.URLFormat)
 }
 
-func (h *Handler) initUrlRoutes(log *slog.Logger, r *chi.Mux) {
+func (h *Handler) initUrlRoutes(log *slog.Logger, r chi.Router) {
 	r.Route("/url", func(r chi.Router) {
-		r.Use(customMiddleware.Authorization(log))
-		r.Post("/save", h.urlSave(log))
-		r.Delete("/{alias}", h.urlDelete(log))
+		r.Get("/{alias}", h.urlRedirect(log))
+		r.Group(func(r chi.Router) {
+			r.Use(customMiddleware.Authorization(log))
+			r.Post("/save", h.urlSave(log))
+			r.Delete("/{alias}", h.urlDelete(log))
+		})
 	})
-	r.Get("/{alias}", h.urlRedirect(log))
+}
+
+func (h *Handler) initUserRoutes(log *slog.Logger, r chi.Router) {
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/signUp", h.userSignUp(log))
+		r.Post("/signIn", h.userSignIn(log))
+		r.Post("/auth/refresh", h.userRefresh(log))
+		r.Group(func(r chi.Router) {
+			r.Use(customMiddleware.Authorization(log))
+			r.Post("/signOut", h.userSignOut(log))
+		})
+	})
 }
